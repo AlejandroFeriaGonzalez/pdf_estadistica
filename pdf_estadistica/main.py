e9fa0acfd141b2d70fadc7f89ef837035141b8f4
alejandro20019 <aferiag@unal.edu.co>
2023-08-09 14:56:53 -0500

    Update main.py

--- pdf_estadistica/main.py
+++ pdf_estadistica/main.py
@@ -1,32 +1,37 @@
-import PyPDF2, re, os
+import PyPDF2
+import re
+import os
 from pathlib import Path
 
 
 ruta_entrada = Path("entrada")
 patron_numDiavositiva = "(\d*)\s/\s(\d*)"
 
+
 def numeroPagina(pagina):
     return re.search(patron_numDiavositiva, pagina.extract_text()).group(1)
+
 
 for d in ruta_entrada.iterdir():
     pdf_reader = PyPDF2.PdfReader(d)
     writer = PyPDF2.PdfWriter()
 
-    numDiavositas =  re.search(patron_numDiavositiva, pdf_reader.pages[-1].extract_text()).group(1)
+    numDiavositas = re.search(patron_numDiavositiva,
+                              pdf_reader.pages[-1].extract_text()).group(1)
 
     for i, page in enumerate(pdf_reader.pages):
         numPag = numeroPagina(page)
 
-        #Si el indice de la siguiente pagina es diferente al actual, entonces es una diavositiva final
-        if (int(numPag) == int(numDiavositas)): break
+        # Si el indice de la siguiente pagina es diferente al actual, entonces es una diavositiva final
+        if (int(numPag) == int(numDiavositas)):
+            break
 
         if (numeroPagina(pdf_reader.pages[i+1]) != numPag):
             writer.add_page(page)
 
-    #añadir ultima pagina
+    # añadir ultima pagina
     writer.add_page(pdf_reader.pages[-1])
 
-    
     ruta_salida = Path(os.path.join("salida", d.name))
     print(f"Creado: {ruta_salida}")
     with open(f"{ruta_salida}.pdf", "wb") as fp:
